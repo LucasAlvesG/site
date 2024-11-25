47c9ae14d02cd68685a5bc3fbc199a1fc25586f7
LucasAlvesG <117421570+LucasAlvesG@users.noreply.github.com>
2024-11-25 19:19:28 -0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -95,7 +95,7 @@ export default function Home() {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 50000,
     rtl: true,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,

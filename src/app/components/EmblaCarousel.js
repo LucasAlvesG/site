be77b2cb210ccf7927ec63bb2b0f171b6ad5cf99
LucasAlvesG <117421570+LucasAlvesG@users.noreply.github.com>
2024-11-25 22:57:49 -0300

    test3

--- src/app/components/EmblaCarousel.js
+++ src/app/components/EmblaCarousel.js
@@ -4,7 +4,9 @@ import Autoplay from 'embla-carousel-autoplay';
 import Image from 'next/image';
 
 const images = [
-  
+  '/carousel7.png',
+  '/carousel5.png',
+  '/carousel9.png',
 ];
 
 export function EmblaCarousel() {
